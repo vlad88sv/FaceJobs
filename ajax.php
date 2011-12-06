ccd90999f5b9e786f765e0faaef374ee4f956429
Vladimir Hidalgo <vlad@todosv.com>
2011-12-06 16:32:24 -0600

    Agregar explicacion de que hace ajax.php

--- ajax.php
+++ ajax.php
@@ -5,4 +5,16 @@ sesion::iniciar_sesion();
 if (!sesion::iniciado())
     return;
 
+    
+  /* El sistema envia via Ajax y codificados en JSON los campos a guardar.
+    * nuestra tarea es asegurar que esos campos se guarden en su tabla respectiva,
+    * los campos vienen adecuadamente codificados por clave y valor, donde la
+    * clave es "tabla.campo" y el valor en base64.
+    * Si recibimos el atributo "lazo=tabla" significa que todos los campos recibidos
+    * van en una solo registro de 1 sola tabla.
+    * todas las tablas sin excepcion cuentan con un ID_cuenta, que no será enviado
+    * si no que será inferido de la $info de sesión. Por seguridad no grabaremos nada
+    * en nombre de otro usuario que no sea el de la sesión, para evitar secuestro de
+    * sesiones.
+   */ 
 ?>
