bf7c7596e6da964f6fb4946d71035d5126f07754
Vladimir Hidalgo <vlad@todosv.com>
2012-01-10 09:41:26 -0600

    Like va a la aplicación de FB, no a la pagina

--- contenido/facebook/cabecera.php
+++ contenido/facebook/cabecera.php
@@ -3,7 +3,7 @@
     <div id="bandera">
     	<div id="imagen">$$FLAG$$</div>
     	<br />
-    	<div class="fb-like" data-href="http://facejobs.org/" data-send="true" data-layout="button_count" data-width="80" data-show-faces="true" data-action="recommend"></div>
+    	<div class="fb-like" data-href="http://apps.facebook.com/facejobs_org" data-send="true" data-layout="button_count" data-width="80" data-show-faces="true" data-action="recommend"></div>
     </div>
     <div id="cabera_pais">La bolsa de trabajo de mayor alcance en $$PAIS$$</div>
 </div>
