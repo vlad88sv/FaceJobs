4c3094e0b02fcd1cfd15604a9fb95bb2d35cbf0b
Vladimir Hidalgo <vlad@todosv.com>
2012-02-07 15:51:44 -0600

    empresas.html 90%

--- contenido/facebook/empresas.php
+++ contenido/facebook/empresas.php
@@ -10,6 +10,21 @@ ob_start();
 $plantilla = new pln();
 $plantilla->procesar('empresas');
 echo $plantilla->pln;
+
+// Ahora cargamos las empresas que concuerden con la selección
+
+$c = 'SELECT * FROM empresa_registro';
+$r = db::consultar ($c);
+
+while ($r && $f = mysql_fetch_assoc($r))
+{
+    echo '<table class="lista_empresas"><tr>';
+    echo '<td class="logo"><img src="'.ui::ObtenerImagen($f['foto_hash'],40,40,true).'" /></td>';
+    echo '<td class="informacion"><div class="ocre">'.$f['nombre_legal'].'</div><div class="gris"><strong>90</strong> puestos disponibles</div></td>';
+    echo '<td class="accion"><div class="boton"><a href="#">Ver</a></td>';
+    echo '</tr></table>';
+}
+
 $buffer = ob_get_clean();
 
 CrearPlantillaGeneral($buffer);
