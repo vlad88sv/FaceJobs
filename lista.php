734bea3ee350da201262cd9a3e1b7bbef0053a67
Vladimir Hidalgo <vlad@todosv.com>
2012-02-22 13:17:58 -0600

    Procesador de Lista de Puestos y origen

---
+++ lista.php
@@ -0,0 +1,27 @@
+<?php
+    if (isset($_POST['ingresar']))
+    {
+        $grupo = '';
+        $subgrupo = '';
+        foreach (preg_split("/((\r?\n)|(\r\n?))/", $_POST['lista']) as $line_num => $line) {
+                if ($line[0] == "=")
+                {
+                    $grupo = str_replace('=', '', $line);
+                    continue;
+                }
+                if ($line[0] == "[")
+                {
+                    $subgrupo = substr($line, 1, -1);
+                    continue;
+                }
+                
+                $line = str_replace('* ', '', $line);
+            
+            echo "<pre>INSERT INTO datos_puesto (grupo, subgrupo, puesto) VALUES ('". $grupo ."','". $subgrupo ."','". $line ."');</pre>\n";
+        }
+    }
+?>
+<form action="./lista.php" method="post">
+<textarea style="width:100%;" name="lista"></textarea>
+<div><input type="submit" name="ingresar" value="Procesar" /></div>
+</form>
