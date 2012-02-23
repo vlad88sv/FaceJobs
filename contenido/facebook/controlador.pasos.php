e47da68923a3070fd4be5b1a11b73726fa112fff
Vladimir Hidalgo <vlad@todosv.com>
2012-02-23 15:28:02 -0600

    Edicion de perfil por pasos queda descontinuado

--- contenido/facebook/controlador.pasos.php
+++ contenido/facebook/controlador.pasos.php
@@ -22,24 +22,10 @@ if (!is_numeric($paso) || $paso < 1 || $paso > 9)
     $paso = 1;
 }
 
-body::agregarAlContenido('ver.pasos',true);
-
 general::requerirModulo(array('plantilla','cv'));
 
-general::registrarEstiloCSS('FaceBox','facebox');
-
-general::registrarScriptJS('fileuploader','fileuploader');
-general::registrarScriptJS('FaceBox','jquery.facebox');
-
 $plantilla = new pln();
-
 $plantilla->procesar('pasos/paso_'.$paso);
 
 body::agregarContenidoAlContenido($plantilla->pln);
-body::agregarContenidoAlContenido('<div id="controlador_pasos">');
-if ($paso > 1)
-    body::agregarContenidoAlContenido('<div id="paso_anterior"><a href="'.PROY_URL.'paso.html?p='.(max(($paso-1),1)).'"><img src="img/paso_anterior.png" />Anterior</a></div>');
-if ($paso < 9)
-    body::agregarContenidoAlContenido('<div id="paso_siguiente"><a href="'.PROY_URL.'paso.html?p='.(min(($paso+1),9)).'">Siguiente<img src="img/paso_siguiente.png"  /></a></div>');
-body::agregarContenidoAlContenido('</div>');
 ?>
