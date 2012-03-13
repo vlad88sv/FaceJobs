6c4caba78aa25be1b43d960b35ae41d233805f1d
Vladimir Hidalgo <vlad@todosv.com>
2012-03-13 14:32:16 -0600

    Verbose

--- sync.sh
+++ sync.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync --compress-level=9 --exclude '*.psd' --exclude '.git' -a  ./  root@vps.mupi.com.sv:/var/www/facejobs.org/
+rsync -v --compress-level=9 --exclude '*.psd' --exclude '.git' -a  ./  root@vps.mupi.com.sv:/var/www/facejobs.org/
