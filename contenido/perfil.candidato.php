7f082d7a0f5bce48c353536e5e3a704de216a08c
Vladimir Hidalgo <vladimiroski@gmail.com>
2012-04-18 21:22:09 -0600

    OSCAR: botón guardar en ventanas de edición
    Signed-off-by: Vladimir Hidalgo <vladimiroski@gmail.com>

--- contenido/perfil.candidato.php
+++ contenido/perfil.candidato.php
@@ -13,7 +13,8 @@ echo pln::$pln;
 <script type="text/javascript">
     $(function(){
     	 $('.editarPaso').facebox();
-         $(document).bind('afterClose.facebox', function() { window.location.href=window.location.href; });         
+         $(document).bind('afterClose.facebox', function() { window.location.href=window.location.href; });
+	 $('.cerrar_facebox').live('click',function () {$(document).trigger('close.facebox');});
          plnJS_iniciar();
     });
 </script>
